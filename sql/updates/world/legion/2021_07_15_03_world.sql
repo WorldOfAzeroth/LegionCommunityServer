1d8fa658e469d5eacbeb903000534fd0cd56bf77
Sturm <t4cpirates@hotmail.com>
2021-07-27 20:29:55 +0000

    Update 2021_07_15_03_world.sql

--- sql/updates/world/legion/2021_07_15_03_world.sql
+++ sql/updates/world/legion/2021_07_15_03_world.sql
@@ -1,6 +1,6 @@
 -- Fixes issues 16, 18, 21, 22, 24, 25, 26, 27, 56
 
-DELETE FROM `gossip_menu_option_trainer` WHERE MenuId in (4128, 4165, 4242, 4351, 4185, 5854, 4349, 2021);
+DELETE FROM `gossip_menu_option_trainer` WHERE MenuId in (4128, 4165, 4242, 4351, 4185, 5854, 4349, 6286);
 
 INSERT INTO `gossip_menu_option_trainer` (`MenuId`, `OptionIndex`, `TrainerId`) VALUES 
 -- Bena Winterhoof, Thnderbluff alchemy trainer gossip fix. ( Closes issue #27 =)
@@ -18,7 +18,7 @@ INSERT INTO `gossip_menu_option_trainer` (`MenuId`, `OptionIndex`, `TrainerId`) 
 -- Me'lynn, Darnassus tailoring trainer fix. ( Closes issue #18 )
 (4349, 0, 163),
 -- Zarrin, Teldrassil cooking trainer fix. ( Closes issue #16 )
-(2021, 0, 136);
+(6286, 0, 136);
 
 UPDATE `creature_template` SET `npcflag` = 17 WHERE `creature_template`.`entry` in (3011, 3004, 3069, 3067, 4159, 6286);
 UPDATE `creature_template` SET `npcflag` = 19 WHERE `creature_template`.`entry` in (3009, 3007 );
