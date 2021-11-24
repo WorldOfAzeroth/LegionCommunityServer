bd09ff6b8c5b2df4f0e963c8fff51959fb15edbd
Sturm <t4cpirates@hotmail.com>
2021-11-24 19:31:34 +0000

    Update 2021_24_11_01_world.sql

--- sql/updates/world/legion/2021_24_11_01_world.sql
+++ sql/updates/world/legion/2021_24_11_01_world.sql
@@ -1,3 +1,4 @@
 -- Engineering Trainer Tinkerwiz, Rathchet
 
+DELETE FROM gossip_menu_option_trainer WHERE `MenuId` = 4140;
 INSERT INTO gossip_menu_option_trainer (`MenuId`, `OptionIndex`, `TrainerId`) VALUES (4140, 0, 407);
