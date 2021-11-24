d8c57bade7f63b5a8107aa18458cf3587defa584
Sturm <t4cpirates@hotmail.com>
2021-11-24 18:08:54 +0000

    DB/Quest The Future of Gnomeregan

---
+++ sql/updates/world/legion/2021_24_11_02_world.sql
@@ -0,0 +1,4 @@
+-- Quest: The Future of Gnomeregan ( Monk )
+
+DELETE FROM creature_queststarter WHERE `id` = 42396 AND `quest` = 31135;
+INSERT INTO creature_queststarter (`id`, `quest`) VALUES (42396, 31135);
