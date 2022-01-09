6561e207c2dc87b6552874a203d1667c6cdc407a
aquadeus <efectokoleos13@gmail.com>
2022-01-09 22:27:20 +0100

    Core/Guilds: Implemented reordering guild ranks
    (cherry picked from commit 9dc11391c570ebc75a580aceed0c7eeaa1dedcf0)

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -338,6 +338,7 @@ namespace WorldPackets
         class GuildGetRanks;
         class GuildAddRank;
         class GuildDeleteRank;
+        class GuildShiftRank;
         class GuildUpdateInfoText;
         class GuildSetMemberNote;
         class GuildEventLogQuery;
@@ -1362,6 +1363,7 @@ class TC_GAME_API WorldSession
         void HandleGuildSetRankPermissions(WorldPackets::Guild::GuildSetRankPermissions& packet);
         void HandleGuildAddRank(WorldPackets::Guild::GuildAddRank& packet);
         void HandleGuildDeleteRank(WorldPackets::Guild::GuildDeleteRank& packet);
+        void HandleGuildShiftRank(WorldPackets::Guild::GuildShiftRank& shiftRank);
         void HandleGuildUpdateInfoText(WorldPackets::Guild::GuildUpdateInfoText& packet);
         void HandleSaveGuildEmblem(WorldPackets::Guild::SaveGuildEmblem& packet);
         void HandleGuildRequestPartyState(WorldPackets::Guild::RequestGuildPartyState& packet);
