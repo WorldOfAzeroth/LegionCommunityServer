afccec90c7ed6be54b86d98a5c8d0473ea4ead24
Jeremy <Golrag@users.noreply.github.com>
2023-10-08 19:59:21 +0200

    Core/Objects: Added GameObject type parameter to FindGameObjectOptions (#29356)
    (cherry picked from commit 52102a6395bc6244bdcfa55e357425bd5c9aefc4)

--- src/server/game/Grids/Notifiers/GridNotifiers.h
+++ src/server/game/Grids/Notifiers/GridNotifiers.h
@@ -1576,6 +1576,9 @@ namespace Trinity
             if (i_args.IgnoreNotOwnedPrivateObjects && !go->CheckPrivateObjectOwnerVisibility(&i_obj))
                 return false;
 
+            if (i_args.GameObjectType && go->GetGoType() != i_args.GameObjectType)
+                return false;
+
             i_customizer.Update(go);
             return true;
         }
