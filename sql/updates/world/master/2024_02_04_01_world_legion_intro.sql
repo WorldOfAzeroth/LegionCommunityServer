f8600e51c76c534c917ad29a21d1be640ecbfc1a
Jorgie.Li <jorgie@qq.com>
2024-02-05 17:46:41 +0800

    DB/Legion: Game content for Legion

---
+++ sql/updates/world/master/2024_02_04_01_world_legion_intro.sql
@@ -0,0 +1,10 @@
+-- free CGUID
+-- '221146' - '218657';
+-- '250000' - '223866';
+
+SET @CGUID := 400000;
+SET @OGUID := 400610;
+
+
+-- delete FROM `creature` WHERE `guid` BETWEEN @CGUID+0 AND @CGUID+43948;
+-- delete FROM `gameobject` WHERE `guid` BETWEEN @OGUID+0 AND @OGUID+16443;
